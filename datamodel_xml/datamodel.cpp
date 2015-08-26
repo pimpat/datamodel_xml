77dd77d9783abe3e5be77a2861d6829f872905b8
Pimpat <pimpat@pim.macair>
2015-08-26 18:47:32 +0700

    connected transport(threadsafe--strtok_r/new ui)

---
+++ datamodel_xml/datamodel.cpp
@@ -0,0 +1,9 @@
+//
+//  datamodel.cpp
+//  datamodel_xml
+//
+//  Created by Pimpat on 8/24/2558 BE.
+//  Copyright (c) 2558 Pimpat. All rights reserved.
+//
+
+#include "datamodel.h"
---
+++ datamodel_xml/datamodel.h
@@ -0,0 +1,14 @@
+//
+//  datamodel.h
+//  datamodel_xml
+//
+//  Created by Pimpat on 8/24/2558 BE.
+//  Copyright (c) 2558 Pimpat. All rights reserved.
+//
+
+#ifndef __datamodel_xml__datamodel__
+#define __datamodel_xml__datamodel__
+
+#include <stdio.h>
+
+#endif /* defined(__datamodel_xml__datamodel__) */
